12a12d9ae1986a67cc1841b8baf951b9546be337
aaron addleman <aaronaddleman@gmail.com>
2013-11-10 22:02:31 -0800

    new examples

---
+++ childprocess.js
@@ -0,0 +1,9 @@
+// Import the spawn function defined on child_process module
+var spawn = require('child_process').spawn; 
+// Launch a child process with a "tail -f /var/log/system.log" command
+var child = spawn('tail', ['-f', '/var/log/system.log']);
+
+// print child output to the console
+child.stdout.on('data', function( data) {
+  console.log('tail output: ' + data); 
+});
---
+++ runcommand.js
@@ -0,0 +1,8 @@
+var exec = require('child_process').exec;
+var options = { 
+  timeout: 10000, 
+  killSignal: 'SIGKILL' 
+};
+exec('cat *.js | wc -l', options, function(err, stdout, stderr) { 
+ console.log(stdout)
+});
